83365e9b618d9a623b18defce85d9ad6e7810fab
Park Ju Hyung <qkrwngud825@gmail.com>
2018-10-12 06:33:28 +0900

    Add charge.sh for maintenance
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

---
+++ system/xbin/charge.sh
@@ -0,0 +1,30 @@
+#!/system/bin/sh
+
+exec > /dev/kmsg 2>&1
+
+export PATH=/system/xbin:$PATH
+
+# Sleep for 3 seconds to allow charging animation to start
+busybox sleep 3
+
+if busybox pgrep e4defrag > /dev/null; then
+  echo "charge.sh: previous run didn't finish yet"
+  exit 1
+fi
+echo "chsh" > /sys/power/wake_lock
+
+busybox sync
+echo "3" > /proc/sys/vm/drop_caches
+
+echo "charge.sh: trimming /data"
+busybox fstrim -v /data
+echo "charge.sh: trimming done"
+
+echo "charge.sh: defragging /data"
+e4defrag /data | busybox tail -n2
+echo "charge.sh: defragging done"
+
+busybox sync
+echo "3" > /proc/sys/vm/drop_caches
+
+echo "chsh" > /sys/power/wake_unlock
